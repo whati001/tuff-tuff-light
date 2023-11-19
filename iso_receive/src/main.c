613cea86fc2e9dbc9a5c8c526475a2a8db3be1de
akarner <andreaskarner@outlook.com>
2023-11-19 13:44:51 +0100

    feat: Added accelerometer assembly test

--- iso_receive/src/main.c
+++ iso_receive/src/main.c
@@ -1,6 +1,7 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/sensor.h>
 
 #define DONGLE 0
 
@@ -38,13 +39,49 @@ void enable_usb_logging()
 #endif
 }
 
+const struct device *const accelerometer = DEVICE_DT_GET(DT_NODELABEL(accelerometer));
+
+static int ttl_acc_enable()
+{
+	if (!device_is_ready(accelerometer)) {
+		LOG_ERR("%s: device not ready.\n", accelerometer->name);
+		return TTL_ERR;
+	}
+
+	struct sensor_value accel[3];
+	while(1)
+	{
+		k_msleep(1000);
+		int ret = sensor_sample_fetch(accelerometer);
+		if (ret < 0)
+		{
+			LOG_ERR("Failed to fetch accel sensor data\n");
+			continue;
+		}
+
+		if (!ret)
+		{
+			LOG_WRN("No data to sample\n");
+			continue;
+		}
+
+		ret = sensor_channel_get(accelerometer, SENSOR_CHAN_ACCEL_XYZ, accel);
+		if (ret < 0)
+		{
+			LOG_ERR("Failed to read accel sensor data\n");
+			continue;
+		}
+		LOG_INF("AccelData[x: %d, y:%d, z:%d]", (int)sensor_value_to_double(&accel[0]), (int)sensor_value_to_double(&accel[1]), (int)sensor_value_to_double(&accel[2]));
+	}
+}
+
 int main(void)
 {
 	int err = 0;
 	LOG_INF("Starting TTLight Controller\n");
 	enable_usb_logging();
 
-	err = ttl_ble_init();
+	// err = ttl_ble_init();
 	if (TTL_OK != err)
 	{
 		LOG_ERR("Failed to initialize the TTLight BLE stack\n");
@@ -56,6 +93,12 @@ int main(void)
 		LOG_ERR("Failed to initialize the TTLight GPIO stack\n");
 	}
 	ttl_ble_register_cb(ttl_led_upd_status);
+
+	err = ttl_acc_enable();
+	if (TTL_OK != err)
+	{
+		LOG_ERR("Failed to initialize the TTLight Acceleromater stack\n");
+	}
 
 	return 0;
 }
